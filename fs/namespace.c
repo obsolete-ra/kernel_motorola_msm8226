9ff50d4f20c090a8801b4dfb7285d8115c160679
Al Viro <viro@zeniv.linux.org.uk>
2012-10-11 11:42:01 -0400

    consitify do_mount() arguments
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    (cherry picked from commit 808d4e3cfdcc52b19276175464f6dbca4df13b09)
    
    Conflicts:
    	fs/namespace.c

--- fs/namespace.c
+++ fs/namespace.c
@@ -1893,7 +1893,8 @@ unlock:
  * create a new mount for userspace and request it to be added into the
  * namespace's tree
  */
-static int do_new_mount(struct path *path, const char *fstype, int flags,
+
+static int do_new_mount(struct path *path, const char *type, int flags,
 			int mnt_flags, const char *name, void *data)
 {
 	struct file_system_type *type;
