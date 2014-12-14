a9de6a256ac8a59984034b5521bdab7637b4eceb
Luka Karinja <lk@varjanta.com>
2014-12-14 20:38:16 +0100

    more underclocking all around
    Conflicts:
    	arch/arm/boot/dts/msm8226.dtsi

--- arch/arm/mach-msm/acpuclock-8226.c
+++ arch/arm/mach-msm/acpuclock-8226.c
@@ -62,7 +62,7 @@ static struct msm_bus_scale_pdata bus_client_pdata = {
 static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p1[] = {
 
 #ifdef CPU_UNDERCLOCK
-	{ 1,  192000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
+	{ 1,  192000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 3 },
 #endif
 	{ 1,  300000, PLL0,    4, 2,   CPR_CORNER_2,    0, 3 },
 	{ 1,  384000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
@@ -78,7 +78,7 @@ static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p1[] = {
 static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p2[] = {
 
 #ifdef CPU_UNDERCLOCK
-	{ 1,  192000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
+	{ 1,  192000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 3 },
 #endif
 	{ 1,  300000, PLL0,    4, 2,   CPR_CORNER_2,    0, 3 },
 	{ 1,  384000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
@@ -94,7 +94,7 @@ static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p2[] = {
 static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p4[] = {
 
 #ifdef CPU_UNDERCLOCK
-	{ 1,  192000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
+	{ 1,  192000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 3 },
 #endif
 	{ 1,  300000, PLL0,    4, 2,   CPR_CORNER_2,    0, 3 },
 	{ 1,  384000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
@@ -113,7 +113,7 @@ static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p4[] = {
 static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p6[] = {
 
 #ifdef CPU_UNDERCLOCK
-	{ 1,  192000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
+	{ 1,  192000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 3 },
 #endif
 	{ 1,  300000, PLL0,    4, 2,   CPR_CORNER_2,    0, 3 },
 	{ 1,  384000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
