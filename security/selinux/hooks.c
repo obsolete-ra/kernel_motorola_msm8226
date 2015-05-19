ba0c129320aac3cddad88f297508dbf6e54b209b
Stephen Smalley <sds@tycho.nsa.gov>
2015-05-19 13:59:12 -0400

    selinux: enable per-file labeling for debugfs files.
    upstream commit 6f29997f4a3117169eeabd41dbea4c1bd94a739c
    
    Add support for per-file labeling of debugfs files so that
    we can distinguish them in policy.  This is particularly
    important in Android where certain debugfs files have to be writable
    by apps and therefore the debugfs directory tree can be read and
    searched by all.
    
    Since debugfs is entirely kernel-generated, the directory tree is
    immutable by userspace, and the inodes are pinned in memory, we can
    simply use the same approach as with proc and label the inodes from
    policy based on pathname from the root of the debugfs filesystem.
    Generalize the existing labeling support used for proc and reuse it
    for debugfs too.
    
    [sds:  Back-ported to 3.4.  superblock_security_struct flags field
    is only unsigned char in 3.4 so we have to redefine SE_SBGENFS.
    However, this definition is kernel-private, not exposed to userspace
    or stored anywhere persistent.  Also depends on
    I7742b4b7e53b45e4dd13d99c39553a927aa4a7e9.]
    
    Change-Id: I6460fbed6bb6bd36eb8554ac8c4fdd574edf3b07
    Signed-off-by: Stephen Smalley <sds@tycho.nsa.gov>
    
    Conflicts:
    	security/selinux/hooks.c

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -696,9 +696,7 @@ static int selinux_set_mnt_opts(struct super_block *sb,
 	if (strcmp(sb->s_type->name, "proc") == 0)
 		sbsec->flags |= SE_SBPROC | SE_SBGENFS;
 
-	if (!strcmp(sb->s_type->name, "debugfs") ||
-	    !strcmp(sb->s_type->name, "sysfs") ||
-	    !strcmp(sb->s_type->name, "pstore"))
+	if (strcmp(sb->s_type->name, "debugfs") == 0)
 		sbsec->flags |= SE_SBGENFS;
 
 	/* Determine the labeling behavior to use for this filesystem type. */
@@ -1338,7 +1336,7 @@ static int inode_doinit_with_dentry(struct inode *inode, struct dentry *opt_dent
 		/* Default to the fs superblock SID. */
 		isec->sid = sbsec->sid;
 
-		if ((sbsec->flags & SE_SBPROC) && !S_ISLNK(inode->i_mode)) {
+		if ((sbsec->flags & SE_SBGENFS) && !S_ISLNK(inode->i_mode)) {
 			/* We must have a dentry to determine the label on
 			 * procfs inodes */
 			if (opt_dentry)
@@ -1361,7 +1359,10 @@ static int inode_doinit_with_dentry(struct inode *inode, struct dentry *opt_dent
 			if (!dentry)
 				goto out_unlock;
 			isec->sclass = inode_mode_to_security_class(inode->i_mode);
-			rc = selinux_proc_get_sid(dentry, isec->sclass, &sid);
+
+			rc = selinux_genfs_get_sid(dentry, isec->sclass,
+						   sbsec->flags, &sid);
+
 			dput(dentry);
 			if (rc)
 				goto out_unlock;
