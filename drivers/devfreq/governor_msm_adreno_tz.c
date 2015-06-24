8719df1a0dae854744410db4e8605b1eef975769
anarkia1976 <stefano.villa1976@gmail.com>
2015-06-24 17:24:38 +0200

    devfreq: adreno_idler: Clean up & Use state notifier helper
    * Thanks to neobuddy89 & @Dorimanx
    * To detect suspend status
    * Disable adreno idler by default.
    * Clean up code implementation - add header file for externs.
    * Merge updates from @Dorimanx.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/devfreq/governor_msm_adreno_tz.c
+++ drivers/devfreq/governor_msm_adreno_tz.c
@@ -37,6 +37,15 @@ static DEFINE_SPINLOCK(tz_lock);
 #define HIST			5
 #define TARGET			80
 #define CAP			75
+
+/*
+ * Use BUSY_BIN to check for fully busy rendering
+ * intervals that may need early intervention when
+ * seen with LONG_FRAME lengths
+ */
+#define BUSY_BIN		95
+#define LONG_FRAME		25000
+
 /*
  * CEILING is 50msec, larger than any standard
  * frame length, but less than the idle timer.
